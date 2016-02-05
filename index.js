d82f6840863bf745180ebe18b3049a0548516d2b
nicola <notsecurity@gmail.com>
2016-02-05 13:33:33 -0500

    adding default app settings

---
+++ index.js
@@ -0,0 +1,26 @@
+const ldnode = require('ldnode')
+const express = require('express')
+const fs = require('fs')
+const https = require('https')
+
+const PORT = process.env.PORT || 8443
+const KEY = process.env.KEY || './certs/key.pem'
+const CERT = process.env.CERT || './certs/key.pem'
+const options = {
+  key: fs.readFileSync(KEY),
+  cert: fs.readFileSync(CERT)
+}
+
+var app = express()
+var solid = ldnode({
+  webid: true,
+  idp: true,
+  defaultApp: false,
+  root: './data'
+})
+
+app.use('/', solid)
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log('Ldnode has started')
+})
